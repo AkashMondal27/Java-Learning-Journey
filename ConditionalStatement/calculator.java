0c28be9e9411e3c6bca90f48577d7160dba997e7
AkashMondal27 <akashmondal102003@gmail.com>
2025-09-26 10:39:27 +0530

    Enhance calculator program with user prompts for input and error handling for division by zero

--- ConditionalStatement/calculator.java
+++ ConditionalStatement/calculator.java
@@ -1,10 +1,41 @@
 import java.util.*;
+
 public class calculator {
     public static void main(String[] args) {
-       try(Scanner sc = new Scanner(System.in)){
-        int a =sc.nextInt();
-        int b = sc.nextInt();
-        char operators = sc.next().charAt(0);
-       }
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the first number : ");
+            int a = sc.nextInt();
+             System.out.println("Enter the second number : ");
+            int b = sc.nextInt();
+             System.out.println("Enter the operator: ");
+            char operator = sc.next().charAt(0);
+
+            int result ;
+
+            switch (operator) {
+                case '+':
+                    result = a + b;
+                    break;
+                case '-':
+                    result = a - b;
+                    break;
+                case '*':
+                    result = a * b;
+                    break;
+                case '/':
+                    if (b != 0) {
+                        result = a / b;
+                    } else {
+                        System.out.println("Error: Division by zero");
+                        return;
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid operator");
+                    return;
+            }
+
+            System.out.println("Result: " + result);
+        }
     }
 }
