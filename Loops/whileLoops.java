c845725700390f14883bec166c96ab3b59473ddb
AkashMondal27 <akashmondal102003@gmail.com>
2025-09-27 08:31:51 +0530

    Add printnumber and whileLoops Java programs for basic loop demonstrations

---
+++ Loops/whileLoops.java
@@ -0,0 +1,12 @@
+
+public class whileLoops {
+    public static void main(String[] args) {
+        int count = 0;
+
+        while (count < 5) {
+            System.out.println("GOOD MORNING");
+            count++;
+        }
+    }
+}
+
