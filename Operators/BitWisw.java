bb250748bf77c2a563ef845bacf1664013b502e7
AkashMondal27 <akashmondal102003@gmail.com>
2025-10-29 11:31:12 +0530

    Add Java program for demonstrating bitwise operations

---
+++ Operators/BitWisw.java
@@ -0,0 +1,22 @@
+public class BitWisw {
+    public static void main(String[] args) {
+        
+        //(1) Binary AND &
+        System.out.println( " 5 & 6 = " + (5 & 6) ); 
+         //(2) Binary QR |
+        System.out.println( " 5 | 6 = " + (5 | 6));  
+         //(3) Binary XQR ^
+        System.out.println( " 5 ^ 6 = " + (5 ^ 6));  
+         //(4) Binary Ones's Compliment ~
+        System.out.println( " ~5 = " + (~5));  
+        //(5) Binary left shift <<
+        System.out.println( " 5 <<2  = " + (5<<2)); 
+        //(5) Binary right shift >>
+        System.out.println( " 5 <<2  = " + (5>>2));  
+         
+        
+        
+
+        
+    }
+}
