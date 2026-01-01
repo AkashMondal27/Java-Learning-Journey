abc8d1448159e676d56f5d403ca8146219ca947a
AkashMondal27 <akashmondal102003@gmail.com>
2026-01-02 00:13:59 +0530

    Add Java program to print all even numbers from 1 to 100 using a while loop

---
+++ Loops/question6.java
@@ -0,0 +1,15 @@
+//Print all even numbers from 1 to 100 using while loop.
+public class question6 {
+    
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
+
+
