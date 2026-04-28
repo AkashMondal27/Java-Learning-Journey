6dc14892a2fc633cbb9f2fe0e6d2ccbd83991b8d
AkashMondal27 <akashmondal102003@gmail.com>
2026-04-28 10:42:24 +0530

    defalut A.M.

---
+++ OPPS/AccessModifier/Default/example.java
@@ -0,0 +1,21 @@
+
+ class example {
+   public static void main(String[] args) {
+       example  obj= new example() ;  // create the object 
+        System.out.println("Name :" +obj.name);     
+        System.out.println("Age :" +obj.age);
+        System.out.println("Email :" +obj.email);
+     }
+
+
+
+
+
+    //creta the private class
+    String name = "AKASH MONDAL";  // we can't access this variable outside the class 
+    int age = 25;
+     String email = "akak@gmail.com";
+}
+
+
+    
