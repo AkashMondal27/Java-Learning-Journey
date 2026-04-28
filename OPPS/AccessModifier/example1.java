9f591894861b2a3d3fc8892df742b4bbfecc169a
AkashMondal27 <akashmondal102003@gmail.com>
2026-04-28 08:36:21 +0530

    Remove commented-out password handling in BankAccount class

--- OPPS/AccessModifier/example1.java
+++ OPPS/AccessModifier/example1.java
@@ -5,7 +5,7 @@ public class example1 {
     public static void main(String[] args) {
         BankAccount account1= new BankAccount();
         account1.username ="AKASH MONDAL";
-        account1.setPassword("12344");
+        // account1.setPassword("12344");
 
             System.out.println("Username is " + account1.username);
              // System.out.println("Password is " + account1.password);  // we can't access private variable password outside the class BankAccount
@@ -18,9 +18,9 @@ public class example1 {
 
 class BankAccount {
     public String username;
-    private String password ;
+    // private String password ;
 
-    public void setPassword ( String newPassword){  //we can accress private variable passeord inside the same class .
-        password = newPassword;  
-    }
+    // public void setPassword ( String newPassword){  //we can accress private variable passeord inside the same class .
+    //     password = newPassword;  
+    // }
 }
