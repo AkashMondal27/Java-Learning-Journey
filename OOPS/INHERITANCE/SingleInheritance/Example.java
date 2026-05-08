122f077243dae9d67bd98b5a8741ad06deb089c7
AkashMondal27 <akashmondal102003@gmail.com>
2026-05-08 15:04:58 +0530

    single Inheritance

---
+++ OOPS/INHERITANCE/SingleInheritance/Example.java
@@ -0,0 +1,27 @@
+public class Example {
+   public static void main(String[] args) {
+        // Creating object of subclass invokes base class constructor
+        Car obj = new Car();
+    }
+}
+
+//Super class
+class Vehicle {
+    Vehicle() {  // this is constructor so the name is sane is class name 
+        System.out.println("This is a Vehicle");
+    }
+}
+
+// Subclass 
+class Car extends Vehicle {
+    Car() {
+        System.out.println("This Vehicle is Car");
+    }
+}
+
+
+/* ..............NOTES ....................
+1. Single Inheritance
+In single inheritance, a sub-class is derived from only one super class.
+ It inherits the properties and behavior of a single-parent class.
+  Sometimes, it is also known as simple inheritance. */
