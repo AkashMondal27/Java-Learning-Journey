ba3b21f18f495d78bcc14f1dc281c547f108f95e
AkashMondal27 <akashmondal102003@gmail.com>
2026-05-15 09:46:47 +0530

    Eaxple of Interfaces .

---
+++ OOPS/Interfaces/Defination/Example.java
@@ -0,0 +1,37 @@
+//create an interface and implement it in different classes
+interface  ChessPlayer{
+    void Moves();
+}
+
+
+//implementing interface in different classes
+class Queen implements  ChessPlayer{
+   public  void Moves(){
+   System.out.println("up , down , left , right and diagonal(all direction)");
+    }
+}
+
+class Rook implements ChessPlayer{
+    public void Moves(){
+        System.out.println("up , down , left and right (all direction except diagonal)");
+    }
+}
+
+class King implements ChessPlayer{
+    public void Moves(){
+        System.out.println("left , right , up and down (one step only)");
+    }}
+
+public class Example {
+    public static void main(String[] args) {
+        Queen q = new Queen();
+        q.Moves();
+        Rook r = new Rook();
+        r.Moves();
+    }
+}
+
+
+/*  ..............NOTES..............................
+
+Interface :-  In Java Interfaces is blue print of a classs that is contains abstruct methods .*/
